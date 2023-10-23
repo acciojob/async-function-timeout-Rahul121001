5815420bfd010921f96e045de6d9c30fa0c77851
Rahul121001 <139845999+Rahul121001@users.noreply.github.com>
2023-10-23 20:13:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', async function() {
+    const text = document.getElementById('text').value;
+    const delay = parseInt(document.getElementById('delay').value);
+
+    if (text && !isNaN(delay)) {
+        await sleep(delay);
+        document.getElementById('output').textContent = text;
+    }
+});
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
